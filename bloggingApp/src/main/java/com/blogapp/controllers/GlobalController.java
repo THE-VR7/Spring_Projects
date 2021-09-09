000956179a9bd4ec934ed9a68b432e7214179621
Vineet Rathor <35703327+THE-VR7@users.noreply.github.com>
2021-09-09 12:38:52 +0530

    create bloggin project

---
+++ bloggingApp/src/main/java/com/blogapp/controllers/GlobalController.java
@@ -0,0 +1,18 @@
+package com.app.blog.controllers;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class GlobalController extends EntitiyHawk {
+
+}
---
+++ bloggingApp/src/main/java/com/blogapp/controllers/UserController.java
@@ -0,0 +1,16 @@
+package com.app.blog.controllers;
+
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+… */
+@RestController
+@RequestMapping("/")
+public class UserController extends EntitiyHawk {
+
+}
